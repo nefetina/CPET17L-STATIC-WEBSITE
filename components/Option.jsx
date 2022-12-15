23bc885523cf19eb7343c5679953adf8095fff63
nefetina <nefetinaira.tomas@gsfe.tupcavite.edu.ph>
2022-12-15 22:24:24 +0800

    Add Files

--- components/Option.jsx
+++ components/Option.jsx
@@ -11,51 +11,51 @@ const fourth = () => {
                     <picture>
                         <img src = "/image/tomas.png" alt="none"/>
                     </picture>
-                <div className={styles.description}>
-                     <h4><b>NEFETINA IRA TOMAS</b></h4>
-                     <p>Project Manager</p>
-                     <p>BET-COET-4B</p>
-                </div>
+                    <div className={styles.description}>
+                        <h4><b>NEFETINA IRA TOMAS</b></h4>
+                        <p>Project Manager</p>
+                        <p>BET-COET-4B</p>
+                    </div>
                 </div>
                 <div className = {styles.develop}>
                     <picture>
                         <img src = "/image/corpuz.png" alt="none"/>
                     </picture>
-                <div className={styles.description}>
-                     <h4><b>JOYCELYN CORPUZ</b></h4>
-                     <p>Developer</p>
-                     <p>BET-COET-4B</p>
-                </div>
+                    <div className={styles.description}>
+                        <h4><b>JOYCELYN CORPUZ</b></h4>
+                        <p>Developer</p>
+                        <p>BET-COET-4B</p>
+                    </div>
                 </div>
                 <div className = {styles.develop}>
                     <picture>
                         <img src = "/image/miole.png" alt="none"/>
                     </picture>
-                <div className={styles.description}>
-                     <h4><b>CHRICHELLE MIOLE</b></h4>
-                     <p>Designer</p>
-                     <p>BET-COET-4B</p>
-                </div>
+                    <div className={styles.description}>
+                        <h4><b>CHRICHELLE MIOLE</b></h4>
+                        <p>Designer</p>
+                        <p>BET-COET-4B</p>
+                    </div>
                 </div>
                 <div className = {styles.develop}>
                     <picture>
                         <img src = "/image/ramos.png" alt="none"/>
                     </picture>
-                <div className={styles.description}>
-                     <h4><b>DANIELA RAMOS</b></h4>
-                     <p>Developer</p>
-                     <p>BET-COET-4B</p>
-                </div>
+                    <div className={styles.description}>
+                        <h4><b>DANIELA RAMOS</b></h4>
+                        <p>Developer</p>
+                        <p>BET-COET-4B</p>
+                    </div>
                 </div>
                 <div className = {styles.develop}>
                     <picture>
                         <img src = "/image/mico.png" alt="none"/>
                     </picture>
-                <div className={styles.description}>
-                     <h4><b>MICO RYAN SAN PABLO</b></h4>
-                     <p>Designer</p>
-                     <p>BET-COET-4B</p>
-                </div>
+                    <div className={styles.description}>
+                        <h4><b>MICO RYAN SAN PABLO</b></h4>
+                        <p>Designer</p>
+                        <p>BET-COET-4B</p>
+                    </div>
                 </div>
             </div>
         </div>
