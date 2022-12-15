864351d53833c52b9f1b2dc83b4dd185fa6b4629
nefetina <nefetinaira.tomas@gsfe.tupcavite.edu.ph>
2022-12-15 22:07:42 +0800

    Add Files

--- components/Deploy.jsx
+++ components/Deploy.jsx
@@ -11,7 +11,7 @@ const Dep = () => {
                         
                         <h1>What is Web Deploy?</h1>
                         <p>Web Deploy is an extensible client-server tool for syncing content and configuration to IIS. Web Deploy is used primarily in two scenarios:
-                        Developers use it to sync (aka 'publish') a compiled web applications (ASP .Net, PHP etc) from developer tools (Visual Studio, WebMatrix, etc) to IIS
+                        Developers use it to sync (aka `publish`) a compiled web applications (ASP .Net, PHP etc) from developer tools (Visual Studio, WebMatrix, etc) to IIS
                         IT professionals use it to migrate websites & applications from an operating system running an older version of IIS such as IIS6 to an operating system running a newer version of IIS such as IIS 7.5.</p>
                     </div>
             </div>  
